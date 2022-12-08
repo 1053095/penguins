c1599aad9e3aebda216c8485abec3b0fd4e8167f
1053095 <120056704+1053095@users.noreply.github.com>
2022-12-08 21:23:41 +0000

    Delete PenguinProject  directory

--- PenguinProject /Functions/Cleaning.R
+++
@@ -1,8 +0,0 @@
-cleaning <-function(penguins_raw){
-  penguins_raw%>%
-    clean_names()%>%
-    remove_empty(c("rows", "cols")) %>%
-    select(-starts_with("delta")) %>%
-    select(-comments)%>%
-    clean_names()
-}
